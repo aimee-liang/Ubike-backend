6b4b57457d3ad9b065e51722915bad82f3ee6454
aimee-liang <aimeeeliang@gmail.com>
2020-11-17 23:41:13 -0500

    User Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    #skip_before_action :authorized, only: [:create]
+
+    # def profile
+    # render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    # end
+
+    def show
+        user = User.find_by(user_id: params[:user_id])
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+private
+    def user_params
+        params.require(:user).permit(:name, :user_id)
+    end
+
+
+end
