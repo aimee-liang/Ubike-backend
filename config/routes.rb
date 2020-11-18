089f498a37e932acf8ba973ec75929f4a7ea186e
aimee-liang <aimeeeliang@gmail.com>
2020-11-17 21:02:29 -0500

    Updated CORS and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
-  resources :bike_stations, only [:index, :show, :create]
+  resources :bike_stations
   resources :check_ins
   resources :favorite_stations
   resources :reviews
