d78815ddfb8048ceb36115390cfedc3fde5836be
aimee-liang <aimeeeliang@gmail.com>
2020-11-17 21:23:49 -0500

    Reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+    #before_action :authorized
+    def index
+        reviews = Review.all
+        render json: reviews
+    end
+
+    def show
+        review = Review.find_or_create_by(user_id: params[:user_id])
+        render json: review
+    end
+
+    def create
+        review = Review.create(reviews_params)
+        render json: review
+    end
+
+    def destroy
+        review = Review.find(params[:id]).destroy
+        render json: review
+    end
+
+private
+    def reviews_params
+        params.require(:review).permit(:user_id, :bike_station_id, :comment)
+    end
+end
