418174d738c739def6039a3f285df69e3d21e1b6
aimee-liang <aimeeeliang@gmail.com>
2020-11-17 23:07:29 -0500

    Bike stations controller

--- app/controllers/bike_stations_controller.rb
+++ app/controllers/bike_stations_controller.rb
@@ -1,10 +1,24 @@
 class BikeStationsController < ApplicationController
 
     def index
+        bike_stations = Bike_Station.all
+        render json: bike_stations
     end
 
     def show
+        bike_station = Bike_Station.find(bike_station_params)
+        render json: bike_station
     end
 
-    
+    def create
+        bike_station = Bike_Station.find_or_create_by!(bike_station_params)
+        render json: bike_station
+    end
+
+private
+    def bike_station_params
+        params.require(:bike_station).permit!
+    end
+
+
 end
