cdaceed2fc7761a408cb4019e2c67ee31d0db1af
aimee-liang <aimeeeliang@gmail.com>
2020-11-18 16:13:00 -0500

    Updated Users Controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :bike_stations #, only: [:index, :show]
-  resources :check_ins
-  resources :favorite_stations
-  resources :reviews
+  namespace :api do 
+    namespace :v1 do 
+      resources :users
+      resources :bike_stations #, only: [:index, :show]
+      resources :check_ins
+      resources :favorite_stations
+      resources :reviews
+    end
+  end
+  
 end
