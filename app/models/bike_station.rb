5f899b3c2cf0d895a2e9a388bf3e15afebaaef1e
aimee-liang <aimeeeliang@gmail.com>
2020-11-19 12:18:12 -0500

    Additional validation so no more than 8

--- app/models/bike_station.rb
+++ app/models/bike_station.rb
@@ -6,11 +6,20 @@ class BikeStation < ApplicationRecord
     has_many :favorite_stations
 
     validates :available_bike_racks, numericality: { greater_than_or_equal_to: 0}
+    validates :available_bike_racks, numericality: { less_than_or_equal_to: 8}
+    
     validate :available_bike_racks_cannot_be_zero
+    validate :available_bike_racks_cannot_be_more_than_eight
 
     def available_bike_racks_cannot_be_zero
         if available_bike_racks == 0
             errors.add(:available_bike_racks, "There are no bike racks available")
+        end
+    end
+
+    def available_bike_racks_cannot_be_more_than_eight
+        if available_bike_racks >= 8
+            errors.add(:available_bike_racks, "This bike shelter is full")
         end
     end
 
