e1f3ddcec49831cbed3b876241f9a6d90868b370
aimee-liang <aimeeeliang@gmail.com>
2020-11-19 11:17:28 -0500

    validation for bike stations along with error message

--- app/models/bike_station.rb
+++ app/models/bike_station.rb
@@ -4,4 +4,15 @@ class BikeStation < ApplicationRecord
     has_many :users, through: :check_ins
     has_many :users, through: :reviews
     has_many :favorite_stations
+
+    validates :available_bike_racks, numericality: { greater_than_or_equal_to: 0}
+    validates :available_bike_racks_cannot_be_zero
+
+    def available_bike_racks_cannot_be_zero
+        if available_bike_racks == 0
+            errors.add(:available_bike_racks, "There are no bike racks available")
+        end
+    end
+
+
 end
