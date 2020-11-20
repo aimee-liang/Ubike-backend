897b1ff78800bf26d8d609f0561121187d42fbcf
aimee-liang <aimeeeliang@gmail.com>
2020-11-20 14:28:30 -0500

    FavStations skip before action

--- app/controllers/api/v1/favorite_stations_controller.rb
+++ app/controllers/api/v1/favorite_stations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::FavoriteStationsController < ApplicationController
-    #before_action :authorized
+    skip_before_action :authorized
 
     def index
         favorite_stations = FavoriteStation.all
