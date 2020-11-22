7d7027bbb8d3f2a87d271a9aac338c04a9380a0d
aimee-liang <aimeeeliang@gmail.com>
2020-11-22 14:06:52 -0500

    Included methods for check ins and favstations

--- app/serializers/bike_station_serializer.rb
+++ app/serializers/bike_station_serializer.rb
@@ -1,11 +1,16 @@
 class BikeStationSerializer < ActiveModel::Serializer
-    attributes :id, :location, :borough, :available_bike_racks, :number_of_bike_racks, :reviews
-    # has_many :reviews_id, :check_ins, :favorite_stations
+    attributes :id, :location, :borough, :available_bike_racks, :number_of_bike_racks, :reviews, :check_ins, :favorite_stations
 
     def reviews
         self.object.reviews
     end
 
-    #come back to this - check ins and favorite stations
+    def check_ins
+        self.object.check_ins
+    end
+
+    def favorite_stations
+        self.object.favorite_stations
+    end
     
 end  
