ba51c746f104537d5eaf83975fc56e3b788ca5fd
aimee-liang <aimeeeliang@gmail.com>
2020-11-27 15:57:54 -0500

    Included validation on Favorite Station so user can only favorite once

--- app/models/bike_station.rb
+++ app/models/bike_station.rb
@@ -12,6 +12,7 @@ class BikeStation < ApplicationRecord
     validate :available_bike_racks_cannot_be_zero
     validate :available_bike_racks_cannot_be_more_than_sixteen
 
+
     def available_bike_racks_cannot_be_zero
         if available_bike_racks == 0
             errors.add(:available_bike_racks, "There are no bike racks available")
@@ -23,6 +24,7 @@ class BikeStation < ApplicationRecord
             errors.add(:available_bike_racks, "This bike shelter is full")
         end
     end
+
 
 
 end
--- app/models/favorite_station.rb
+++ app/models/favorite_station.rb
@@ -1,4 +1,6 @@
 class FavoriteStation < ApplicationRecord
     belongs_to :user
     belongs_to :bike_station
+
+    validates :user_id, uniqueness: {scope: :bike_station_id, message: "You can't favorite a station more than once!"}
 end
