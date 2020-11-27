5cc25ad10caa6702991faf4fc9ef806102228571
aimee-liang <aimeeeliang@gmail.com>
2020-11-27 12:04:48 -0500

    Working on backend validation for check in to prevent duplicates

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,4 +1,7 @@
 class CheckIn < ApplicationRecord
     belongs_to :user
     belongs_to :bike_station
+
+    # validates :user_id, uniqueness: :true
+    validates :user_id, :presence => true, :uniqueness => true
 end
