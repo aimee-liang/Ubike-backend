399492be6b45e9873b67e795ca1fd45d0656e643
aimee-liang <aimeeeliang@gmail.com>
2020-11-28 22:24:16 -0500

    Added datetime attr to check ins

---
+++ db/migrate/20201129032250_add_date_time_to_check_ins.rb
@@ -0,0 +1,5 @@
+class AddDateTimeToCheckIns < ActiveRecord::Migration[6.0]
+  def change
+    add_column :check_ins, :datetime, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_025056) do
+ActiveRecord::Schema.define(version: 2020_11_29_032250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_025056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "location"
+    t.datetime "datetime"
     t.index ["bike_station_id"], name: "index_check_ins_on_bike_station_id"
     t.index ["user_id"], name: "index_check_ins_on_user_id"
   end
