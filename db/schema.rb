d71fef965fca3a56cfc122971fcd0a8bccb8fa81
aimee-liang <aimeeeliang@gmail.com>
2020-11-29 20:48:34 -0500

    Photo attr in Bike Stations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_032250) do
+ActiveRecord::Schema.define(version: 2020_11_30_014741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_032250) do
     t.integer "available_bike_racks"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photos"
   end
 
   create_table "check_ins", force: :cascade do |t|
